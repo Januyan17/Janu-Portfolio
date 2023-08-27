08cb8a85ac9eebbb92c1bdd5f9c4f0c3abfd9ea7
Januyan <it20223212@my.sliit.lk>
2023-08-28 00:04:03 +0530

    first commit

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { CgCPlusPlus } from "react-icons/cg";
+import {
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiPython,
+  DiGit,
+  DiJava,
+  DiAndroid,
+} from "react-icons/di";
+import { SiFirebase, SiMysql } from "react-icons/si";
+import { TbBrandFlutter, TbBrandGolang, TbBrandKotlin } from "react-icons/tb";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <TbBrandFlutter />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJava />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <TbBrandKotlin />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiAndroid />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      {/* <Col xs={4} md={2} className="tech-icons">
+        <SiNextdotjs />
+      </Col> */}
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFirebase />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMysql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      {/* <Col xs={4} md={2} className="tech-icons">
+        <SiPostgresql />
+      </Col> */}
+    </Row>
+  );
+}
+
+export default Techstack;
