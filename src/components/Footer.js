19eab89056839826b09c211c84d1b2325dc9e43b
Januyan <it20223212@my.sliit.lk>
2024-01-03 19:39:49 +0530

    Resume Updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Developed by Seralagan Januyan</h3>
+          {/* <h3>Developed by Seralagan Januyan</h3> */}
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} SJ</h3>
